dbb7784198de5139765ed5cd39df622f55f27fb7
Sindre Sorhus <sindresorhus@gmail.com>
2018-02-24 23:08:38 +0700

    Show progress in the Dock icon

--- Gifski/util.swift
+++ Gifski/util.swift
@@ -55,6 +55,36 @@ class SSView: NSView {
 }
 
 
+extension NSBezierPath {
+	/// UIKit polyfill
+	var cgPath: CGPath {
+		let path = CGMutablePath()
+		var points = [CGPoint](repeating: .zero, count: 3)
+
+		for i in 0..<elementCount {
+			let type = element(at: i, associatedPoints: &points)
+			switch type {
+			case .moveToBezierPathElement:
+				path.move(to: points[0])
+			case .lineToBezierPathElement:
+				path.addLine(to: points[0])
+			case .curveToBezierPathElement:
+				path.addCurve(to: points[2], control1: points[0], control2: points[1])
+			case .closePathBezierPathElement:
+				path.closeSubpath()
+			}
+		}
+
+		return path
+	}
+	
+	/// UIKit polyfill
+	convenience init(roundedRect rect: CGRect, cornerRadius: CGFloat) {
+		self.init(roundedRect: rect, xRadius: cornerRadius, yRadius: cornerRadius)
+	}
+}
+
+
 extension AVAssetImageGenerator {
 	func generateCGImagesAsynchronously(forTimePoints timePoints: [CMTime], completionHandler: @escaping AVAssetImageGeneratorCompletionHandler) {
 		let times = timePoints.map { NSValue(time: $0) }
