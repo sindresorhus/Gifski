09a77d059235bdaa1ece8bb2220bac4db7d229d4
Sential <sentialx@gmail.com>
2019-01-29 18:57:40 +0100

    Remove unused NSImage resize method

--- Gifski/util.swift
+++ Gifski/util.swift
@@ -1391,20 +1391,6 @@ extension Result {
 	}
 }
 
-extension NSImage {
-	func resize(withSize targetSize: NSSize) -> NSImage? {
-		let frame = NSRect(x: 0, y: 0, width: targetSize.width, height: targetSize.height)
-		guard let representation = self.bestRepresentation(for: frame, context: nil, hints: nil) else {
-			return nil
-		}
-		let image = NSImage(size: targetSize, flipped: false, drawingHandler: { (_) -> Bool in
-			return representation.draw(in: frame)
-		})
-
-		return image
-	}
-}
-
 final class Once {
 	private var lock = os_unfair_lock()
 	private var hasRun = false
