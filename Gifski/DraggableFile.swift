80339ff9d414c5594366c76ee32542cde911fe6c
Sindre Sorhus <sindresorhus@gmail.com>
2019-01-30 12:29:04 +0700

    Update DraggableFile.swift

--- Gifski/DraggableFile.swift
+++ Gifski/DraggableFile.swift
@@ -1,7 +1,7 @@
 import Cocoa
 
 final class DraggableFile: NSImageView, NSDraggingSource {
-	var mouseDownEvent: NSEvent?
+	private var mouseDownEvent: NSEvent?
 
 	var fileUrl: URL? {
 		didSet {
@@ -12,10 +12,9 @@ final class DraggableFile: NSImageView, NSDraggingSource {
 		}
 	}
 
-	override init(frame frameRect: NSRect) {
-		super.init(frame: frameRect)
+	override init(frame: CGRect) {
+		super.init(frame: frame)
 		isEditable = false
-		
 		unregisterDraggedTypes()
 	}
 
@@ -23,22 +22,22 @@ final class DraggableFile: NSImageView, NSDraggingSource {
 		super.init(coder: coder)
 	}
 
-	override func draw(_ dirtyRect: NSRect) {
+	override func draw(_ dirtyRect: CGRect) {
 		super.draw(dirtyRect)
 	}
 
-	func draggingSession(_: NSDraggingSession, sourceOperationMaskFor _: NSDraggingContext) -> NSDragOperation {
+	func draggingSession(_ session: NSDraggingSession, sourceOperationMaskFor context: NSDraggingContext) -> NSDragOperation {
 		return .copy
 	}
 
-	override func mouseDown(with theEvent: NSEvent) {
-		mouseDownEvent = theEvent
+	override func mouseDown(with event: NSEvent) {
+		mouseDownEvent = event
 	}
 
 	override func mouseDragged(with event: NSEvent) {
-		let mouseDown = mouseDownEvent!.locationInWindow
+		let mouseDownPoint = mouseDownEvent!.locationInWindow
 		let dragPoint = event.locationInWindow
-		let dragDistance = hypot(mouseDown.x - dragPoint.x, mouseDown.y - dragPoint.y)
+		let dragDistance = hypot(mouseDownPoint.x - dragPoint.x, mouseDownPoint.y - dragPoint.y)
 
 		if dragDistance < 3 {
 			return
@@ -47,26 +46,27 @@ final class DraggableFile: NSImageView, NSDraggingSource {
 		guard let image = self.image else {
 			return
 		}
-		
-		let size = NSSize(width: 64, height: 64 * (image.size.height / image.size.width))
-		
-		if let draggingImage = image.resize(withSize: size) {
-			let draggingItem = NSDraggingItem(pasteboardWriter: fileUrl! as NSURL)
-			let draggingFrameOrigin = convert(mouseDown, from: nil)
-			let draggingFrame = NSRect(origin: draggingFrameOrigin, size: draggingImage.size)
-				.offsetBy(dx: -draggingImage.size.width / 2, dy: -draggingImage.size.height / 2)
-			
-			draggingItem.draggingFrame = draggingFrame
-			
-			draggingItem.imageComponentsProvider = {
-				let component = NSDraggingImageComponent(key: NSDraggingItem.ImageComponentKey.icon)
-				
-				component.contents = image
-				component.frame = NSRect(origin: NSPoint(), size: draggingFrame.size)
-				return [component]
-			}
-			
-			beginDraggingSession(with: [draggingItem], event: mouseDownEvent!, source: self)
+
+		let size = CGSize(width: 64, height: 64 * (image.size.height / image.size.width))
+
+		guard let draggingImage = image.resize(withSize: size) else {
+			return
 		}
+
+		let draggingItem = NSDraggingItem(pasteboardWriter: fileUrl! as NSURL)
+		let draggingFrameOrigin = convert(mouseDownPoint, from: nil)
+		let draggingFrame = CGRect(origin: draggingFrameOrigin, size: draggingImage.size)
+			.offsetBy(dx: -draggingImage.size.width / 2, dy: -draggingImage.size.height / 2)
+
+		draggingItem.draggingFrame = draggingFrame
+
+		draggingItem.imageComponentsProvider = {
+			let component = NSDraggingImageComponent(key: .icon)
+			component.contents = image
+			component.frame = CGRect(origin: .zero, size: draggingFrame.size)
+			return [component]
+		}
+
+		beginDraggingSession(with: [draggingItem], event: mouseDownEvent!, source: self)
 	}
 }
