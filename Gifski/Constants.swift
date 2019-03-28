a75b5bd87ecb6d24033610d3a3ba11e79270ce49
Sindre Sorhus <sindresorhus@gmail.com>
2019-03-28 20:56:10 +0700

    Ask for App Store review after 5 successful conversions
    And only ask once!

--- Gifski/Constants.swift
+++ Gifski/Constants.swift
@@ -6,4 +6,5 @@ extension NSColor {
 
 extension Defaults.Keys {
 	static let outputQuality = Defaults.Key<Double>("outputQuality", default: 1)
+	static let totalConversionsCount = Defaults.Key<Int>("totalConversionsCount", default: 0)
 }
