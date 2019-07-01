0e5761cd6a638221e83ff1be3c6f054a36be0cce
Łukasz Mróz <thesunshinejr@gmail.com>
2019-07-01 18:37:38 +0200

    Reident ConversionCompleted controller

--- Gifski/ConversionCompletedViewController.swift
+++ Gifski/ConversionCompletedViewController.swift
@@ -10,33 +10,33 @@ final class ConversionCompletedViewController: NSViewController {
 	@IBOutlet private var saveAsButton: NSButton!
 	@IBOutlet private var shareButton: NSButton!
 	@IBOutlet private var copyButton: NSButton!
-	
+
 	private let draggableFile = DraggableFile()
 	private var conversion: Gifski.Conversion!
 	private var gifUrl: URL!
-	
+
 	convenience init(conversion: Gifski.Conversion, gifUrl: URL) {
 		self.init()
-		
+
 		self.conversion = conversion
 		self.gifUrl = gifUrl
 	}
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+
 		setupUI()
 		setupDropView()
 		setup(url: gifUrl)
 	}
-	
+
 	override func viewDidAppear() {
 		super.viewDidAppear()
-		
+
 		if #available(macOS 10.14, *), defaults[.successfulConversionsCount] == 5 {
 			SKStoreReviewController.requestReview()
 		}
-		
+
 		if #available(macOS 10.14, *), !NSApp.isActive || self.view.window?.isVisible == false {
 			let notification = UNMutableNotificationContent()
 			notification.title = "Conversion Completed"
@@ -45,25 +45,25 @@ final class ConversionCompletedViewController: NSViewController {
 			UNUserNotificationCenter.current().add(request)
 		}
 	}
-	
+
 	private func setupUI() {
 		fileNameLabel.maximumNumberOfLines = 1
 		fileNameLabel.textColor = .labelColor
 		fileNameLabel.font = NSFont.systemFont(ofSize: 14, weight: .semibold)
-		
+
 		fileSizeLabel.maximumNumberOfLines = 1
 		fileSizeLabel.textColor = .secondaryLabelColor
 		fileSizeLabel.font = .systemFont(ofSize: 12)
-		
+
 		draggableFileWrapper.addSubview(draggableFile)
 		draggableFile.constrainEdgesToSuperview()
 	}
-	
+
 	private func setup(url: URL) {
 		draggableFile.fileUrl = url
 		fileNameLabel.text = conversion.video.lastPathComponent
 		fileSizeLabel.text = url.fileSizeFormatted
-		
+
 		// TODO: CustomButton doesn't correctly respect `.sendAction()`
 		shareButton.sendAction(on: .leftMouseDown)
 		shareButton.onAction = { _ in
@@ -74,7 +74,7 @@ final class ConversionCompletedViewController: NSViewController {
 			pasteboard.clearContents()
 			pasteboard.writeObjects([url as NSURL])
 		}
-		
+
 		saveAsButton.onAction = { _ in
 			let inputUrl = self.conversion.video
 			let panel = NSSavePanel()
@@ -86,7 +86,7 @@ final class ConversionCompletedViewController: NSViewController {
 			panel.beginSheetModal(for: self.view.window!) { _ in }
 		}
 	}
-	
+
 	private func setupDropView() {
 		let videoDropController = VideoDropViewController(dropLabelIsHidden: true)
 		add(childController: videoDropController)
@@ -97,37 +97,37 @@ extension ConversionCompletedViewController: QLPreviewPanelDataSource {
 	@IBAction private func quickLook(_ sender: Any) {
 		quickLookPreviewItems(nil)
 	}
-	
+
 	override func quickLook(with event: NSEvent) {
 		quickLookPreviewItems(nil)
 	}
-	
+
 	override func quickLookPreviewItems(_ sender: Any?) {
 		guard let panel = QLPreviewPanel.shared() else {
 			return
 		}
-		
+
 		panel.toggle()
 	}
-	
+
 	override func acceptsPreviewPanelControl(_ panel: QLPreviewPanel!) -> Bool {
 		return true
 	}
-	
+
 	override func beginPreviewPanelControl(_ panel: QLPreviewPanel!) {
 		panel.delegate = self
 		panel.dataSource = self
 	}
-	
+
 	override func endPreviewPanelControl(_ panel: QLPreviewPanel!) {
 		panel.dataSource = nil
 		panel.delegate = nil
 	}
-	
+
 	func numberOfPreviewItems(in panel: QLPreviewPanel!) -> Int {
 		return 1
 	}
-	
+
 	func previewPanel(_ panel: QLPreviewPanel!, previewItemAt index: Int) -> QLPreviewItem! {
 		return gifUrl as NSURL
 	}
@@ -137,7 +137,7 @@ extension ConversionCompletedViewController: QLPreviewPanelDelegate {
 	func previewPanel(_ panel: QLPreviewPanel!, sourceFrameOnScreenFor item: QLPreviewItem!) -> CGRect {
 		return draggableFile.imageView?.boundsInScreenCoordinates ?? .zero
 	}
-	
+
 	func previewPanel(_ panel: QLPreviewPanel!, transitionImageFor item: QLPreviewItem!, contentRect: UnsafeMutablePointer<CGRect>!) -> Any! {
 		return draggableFile.image
 	}
