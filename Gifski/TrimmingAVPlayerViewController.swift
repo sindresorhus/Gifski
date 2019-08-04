be90eec820033228424f5ccfde4218275cd7e3ed
Sindre Sorhus <sindresorhus@gmail.com>
2019-08-04 22:25:39 +0200

    Minor tweaks

--- Gifski/TrimmingAVPlayerViewController.swift
+++ Gifski/TrimmingAVPlayerViewController.swift
@@ -1,6 +1,6 @@
 import AVKit
 
-/// VC containing AVPlayerView and also extending possibilities for trimming(view) customization.
+/// VC containing AVPlayerView and also extending possibilities for trimming (view) customization.
 final class TrimmingAVPlayerViewController: NSViewController {
 	var playerView: TrimmingAVPlayerView {
 		return view as! TrimmingAVPlayerView
@@ -12,7 +12,11 @@ final class TrimmingAVPlayerViewController: NSViewController {
 	private let controlsStyle: AVPlayerViewControlsStyle
 	private let timeRangeDidChange: ((ClosedRange<Double>) -> Void)?
 
-	init(asset: AVURLAsset, controlsStyle: AVPlayerViewControlsStyle = .inline, timeRangeDidChange: ((ClosedRange<Double>) -> Void)? = nil) {
+	init(
+		asset: AVURLAsset,
+		controlsStyle: AVPlayerViewControlsStyle = .inline,
+		timeRangeDidChange: ((ClosedRange<Double>) -> Void)? = nil
+	) {
 		self.asset = asset
 		self.controlsStyle = controlsStyle
 		self.timeRangeDidChange = timeRangeDidChange
@@ -49,8 +53,9 @@ final class TrimmingAVPlayerView: AVPlayerView {
 	private var trimmingObserver: NSKeyValueObservation?
 
 	fileprivate func observeTrimmedTimeRange(_ updateClosure: @escaping (ClosedRange<Double>) -> Void) {
-		// Observing duration seems buggy on Mojave -> once we change min target to Catalina,
-		// observe \.duration instead of \.forwardPlaybackEndTime
+		// Observing `.duration` seems buggy on macOS 10.14.
+		// Once we change minimum target to 10.15,
+		// observe `\.duration` instead of `\.forwardPlaybackEndTime`.
 		timeRangeObserver = player?.currentItem?.observe(\.forwardPlaybackEndTime, options: .new) { item, _ in
 			let startTime = item.reversePlaybackEndTime.seconds
 			let endTime = item.forwardPlaybackEndTime.seconds
@@ -70,7 +75,7 @@ final class TrimmingAVPlayerView: AVPlayerView {
 	}
 
 	fileprivate func hideTrimButtons() {
-		// This method is a collection of hacks, so might be acting funky on different OS versions
+		// This method is a collection of hacks, so it might be acting funky on different OS versions.
 		guard
 			let avTrimView = firstSubview(where: { $0.simpleClassName == "AVTrimView" }, deep: true),
 			let superview = avTrimView.superview
@@ -78,8 +83,8 @@ final class TrimmingAVPlayerView: AVPlayerView {
 			return
 		}
 
-		// First find the constraint for trimView that pins to the left edge of the button
-		// Then replace the left edge of a button with the right edge - this will stretch the trim view
+		// First find the constraints for `avTrimView` that pins to the left edge of the button.
+		// Then replace the left edge of a button with the right edge - this will stretch the trim view.
 		if let constraint = superview.constraints.first(where: {
 			($0.firstItem as? NSView) == avTrimView && $0.firstAttribute == .right
 		}) {
@@ -94,7 +99,7 @@ final class TrimmingAVPlayerView: AVPlayerView {
 			constraint.changing(firstAttribute: .right).isActive = true
 		}
 
-		// Now find buttons that are not images (images are playing controls) and hide them
+		// Now find buttons that are not images (images are playing controls) and hide them.
 		superview.subviews
 			.first { $0 != avTrimView }?
 			.subviews
