46922a5114e256eadbd47143d22e4f61c8fb5032
Koray Koska <koray@koska.at>
2019-11-09 16:29:20 +0100

    Handle share extension error in the extension

--- ShareExtension/ShareViewController.swift
+++ ShareExtension/ShareViewController.swift
@@ -3,12 +3,19 @@ import Cocoa
 final class ShareViewController: NSViewController {
 	override var nibName: NSNib.Name? { "ShareViewController" }
 
+    @IBOutlet private weak var errorLabel: NSTextField!
+    @IBOutlet private weak var errorButtonOk: NSButton!
+
 	// swiftlint:disable:next prohibited_super_call
 	override func loadView() {
 		super.loadView()
 
+		// Make error views invisible
+		errorLabel.isHidden = true
+		errorButtonOk.isHidden = true
+
 		guard let item = (self.extensionContext?.inputItems[0] as? NSExtensionItem)?.attachments?.first else {
-			openMainAppAndPresentError(message: "The shared item does not contain an attachment")
+			presentError(message: "The shared item does not contain an attachment")
 			return
 		}
 
@@ -20,13 +27,13 @@ final class ShareViewController: NSViewController {
 		} else if item.hasItemConformingToTypeIdentifier("com.apple.quicktime-movie") {
 			typeIdentifier = "com.apple.quicktime-movie"
 		} else {
-			openMainAppAndPresentError(message: "The shared item is not in a valid video format")
+			presentError(message: "The shared item is not in a valid video format")
 			return
 		}
 
 		item.loadFileRepresentation(forTypeIdentifier: typeIdentifier) { url, error in
 			guard let url = url else {
-				self.openMainAppAndPresentError(message: error?.localizedDescription ?? "Unknown error")
+				self.presentError(message: error?.localizedDescription ?? "Unknown error")
 				return
 			}
 
@@ -34,7 +41,7 @@ final class ShareViewController: NSViewController {
 			let appIdentifierPrefix = Bundle.main.infoDictionary!["AppIdentifierPrefix"] as! String
 
 			guard let appGroupShareVideUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "\(appIdentifierPrefix).gifski_video_share_group")?.appendingPathComponent(shareUrl) else {
-				self.openMainAppAndPresentError(message: "Could not share video with the main app")
+				self.presentError(message: "Could not share video with the main app")
 				return
 			}
 
@@ -42,14 +49,14 @@ final class ShareViewController: NSViewController {
 			do {
 				try FileManager.default.copyItem(at: url, to: appGroupShareVideUrl)
 			} catch {
-				self.openMainAppAndPresentError(message: error.localizedDescription)
+				self.presentError(message: error.localizedDescription)
 				return
 			}
 
 			guard let gifski = self.createMainAppUrl(
 				queryItems: [URLQueryItem(name: "path", value: shareUrl.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)]
 			) else {
-				self.openMainAppAndPresentError(message: "Could not share video with the main app")
+				self.presentError(message: "Could not share video with the main app")
 				return
 			}
 
@@ -60,10 +67,11 @@ final class ShareViewController: NSViewController {
 		}
 	}
 
-	private func openMainAppAndPresentError(message: String) {
-		let gifski = createMainAppUrl(queryItems: [URLQueryItem(name: "error", value: message)])!
-		NSWorkspace.shared.open(gifski)
-		self.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+	private func presentError(message: String) {
+		errorLabel.isHidden = false
+		errorButtonOk.isHidden = false
+
+		errorLabel.stringValue = message
 	}
 
 	private func createMainAppUrl(queryItems: [URLQueryItem] = []) -> URL? {
@@ -74,4 +82,10 @@ final class ShareViewController: NSViewController {
 
 		return components.url
 	}
+
+    // MARK: - Actions
+
+    @IBAction private func errorButtonOkClicked(_ sender: Any) {
+		self.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+    }
 }
