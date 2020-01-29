575e3a2627460f13b9ba5fceeb192c204c3b0046
Sindre Sorhus <sindresorhus@gmail.com>
2020-01-29 14:59:35 +0700

    Allow setting 60 FPS
    The actual result will be 50 FPS, but I doubt the user will notice.
    
    See: https://github.com/sindresorhus/Gifski/issues/161

--- Gifski/Constants.swift
+++ Gifski/Constants.swift
@@ -1,6 +1,12 @@
 import Cocoa
 import Defaults
 
+struct Constants {
+	static let defaultWindowSize = CGSize(width: 360, height: 240)
+	static let backgroundImage = NSImage(named: "BackgroundImage")!
+	static let allowedFrameRate = 5.0...60.0
+}
+
 extension NSColor {
 	static let themeColor = NSColor.controlAccentColorPolyfill
 
@@ -14,10 +20,4 @@ extension Defaults.Keys {
 	static let outputQuality = Key<Double>("outputQuality", default: 1)
 	static let successfulConversionsCount = Key<Int>("successfulConversionsCount", default: 0)
 	static let loopGif = Key<Bool>("loopGif", default: true)
-}
-
-struct Constants {
-	static let defaultWindowSize = CGSize(width: 360, height: 240)
-	static let backgroundImage = NSImage(named: "BackgroundImage")!
-	static let allowedFrameRate = 5.0...30.0
 }
