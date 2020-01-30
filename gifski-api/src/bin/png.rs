e93dd4054ef4df4aebe9d22caa427220e769d52a
Sindre Sorhus <sindresorhus@gmail.com>
2020-01-30 13:01:18 +0700

    Update libgifski (#175)

--- gifski-api/src/bin/png.rs
+++ gifski-api/src/bin/png.rs
@@ -1,6 +1,6 @@
+use crate::error::*;
 use crate::source::*;
 use gifski::Collector;
-use crate::error::*;
 use std::path::PathBuf;
 
 pub struct Lodecoder {
@@ -10,10 +10,7 @@ pub struct Lodecoder {
 
 impl Lodecoder {
     pub fn new(frames: Vec<PathBuf>, fps: usize) -> Self {
-        Self {
-            frames,
-            fps,
-        }
+        Self { frames, fps }
     }
 }
 
@@ -24,8 +21,7 @@ impl Source for Lodecoder {
 
     fn collect(&mut self, mut dest: Collector) -> BinResult<()> {
         for (i, frame) in self.frames.drain(..).enumerate() {
-            let delay = ((i + 1) * 100 / self.fps) - (i * 100 / self.fps); // See telecine/pulldown.
-            dest.add_frame_png_file(i, frame, delay as u16)?;
+            dest.add_frame_png_file(i, frame, i as f64 / self.fps as f64)?;
         }
         Ok(())
     }
--- gifski-api/src/ordqueue.rs
+++ gifski-api/src/ordqueue.rs
@@ -1,7 +1,7 @@
-use std::collections::BinaryHeap;
-use std::cmp::Ordering;
-use std::sync::mpsc;
 use crate::error::*;
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+use std::sync::mpsc;
 
 pub struct OrdQueue<T> {
     sender: mpsc::SyncSender<ReverseTuple<T>>,
