7e3b8513bc17545ea1f7bad7b57b9d169b3405e8
Sindre Sorhus <sindresorhus@gmail.com>
2020-10-09 03:41:18 +0200

    Workaround macOS 11 issue with decoding some videos
    #202

--- Gifski/Utilities.swift
+++ Gifski/Utilities.swift
@@ -337,13 +337,19 @@ extension AVAssetImageGenerator {
 			case .failed:
 				// TODO: Ideally, we should trim blank frames when initially reading the video in `VideoValidator.swift`, but I don't know a way to detect blank frames. We should still keep this fix even if we find a way to trim as this handles blank frames in the middle of the video.
 				// TODO: Report the `xcrun` bug to Apple if it's still an issue in macOS 11.
-				// We ignore blank frames. A video can sometimes contain blank frames at the start when you record an iOS simulator using `xcrun simctl io booted recordVideo simulator.mp4`.
-				if
-					let error = error as? AVError,
-					error.code == .noImageAtTime
-				{
-					totalCount -= 1
-					break
+				if let error = error as? AVError {
+					// We ignore blank frames. A video can sometimes contain blank frames at the start when you record an iOS simulator using `xcrun simctl io booted recordVideo simulator.mp4`.
+					if error.code == .noImageAtTime {
+						totalCount -= 1
+						break
+					}
+
+					// macOS 11 started throwing decode failed error at the first 0 to 0.2-0.5 seconds of screen recordings. As a workaround, we ignore these. We use 1 second just to be sure.
+					if error.code == .decodeFailed && requestedTime.seconds <= 1 {
+						totalCount -= 1
+						Crashlytics.recordNonFatalError(error: error, userInfo: ["requestedTime": requestedTime.seconds])
+						break
+					}
 				}
 
 				completionHandler(.failure(error!))
